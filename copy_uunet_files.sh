28aa69f35bffab17c806966833bd217bb1b7a47a
Matteo Magnani <matteo.magnani@it.uu.se>
2025-09-14 17:46:29 +0200

    removed C support (no longer needed after removing eclat)

--- copy_uunet_files.sh
+++ copy_uunet_files.sh
@@ -6,9 +6,8 @@ rm -rf eclat
 rm -rf boost
 
 #cp -r ../ext/uunet/src src
-cp -r ../../uunet/src src
+cp -r ../ext/uunet/src src
 cp -r ../ext/uunet/ext/infomap infomap
-cp -r ../ext/uunet/ext/eclat eclat
 cp -r ../ext/uunet/ext/boost boost
 cp ../C++/* .
 
@@ -17,12 +16,10 @@ printf "SOURCES=" > sources
 ls | grep "\.cpp$" >> sources
 find src/* | grep "\.cpp$" >> sources
 find infomap/* | grep "\.cpp$" >> sources
-find eclat/* | grep "\.c$" >> sources
 printf "OBJECTS=" > objects
 ls | grep "\.cpp$" | sed 's/cpp$/o/g' >> objects
 find src/* | grep "\.cpp$" | sed 's/cpp$/o/g' >> objects
 find infomap/* | grep "\.cpp$" | sed 's/cpp$/o/g' >> objects
-find eclat/* | grep "\.c$" | sed 's/c$/o/g' >> objects
 
 cat ../make/Makevars > Makevars
 printf "\n" >> Makevars
